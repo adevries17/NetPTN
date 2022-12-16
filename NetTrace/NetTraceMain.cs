91a44b210770a742f4dca47466b55eb2d6278152
adevries17 <andrew@andrewdevries.net>
2022-12-16 15:28:47 -0600

    begin project

---
+++ NetTrace/NetTraceMain.cs
@@ -0,0 +1,15 @@
+﻿using NetPing;
+using System.Net.NetworkInformation;
+
+namespace NetTrace {
+    internal class NetTraceMain {
+        static void Main(string[] args) {
+            PingReply ping = NetPingMain.SendPing(args[0], ttl: 64);
+
+            if (ping.Status == IPStatus.Success) {
+                Console.WriteLine($"Dest: {ping.Address} | TTL: {ping.Options.Ttl} | Latency: {ping.RoundtripTime} | Time: {DateTime.Now}");
+            }
+
+        }
+    }
+}
