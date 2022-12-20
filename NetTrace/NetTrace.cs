a70c665f822db29b1b7fdb304c2e0b79c28e88e6
adevries17 <andrew@andrewdevries.net>
2022-12-20 16:01:02 -0600

    making some progress

--- NetTrace/NetTrace.cs
+++ NetTrace/NetTrace.cs
@@ -9,9 +9,41 @@ namespace NetTrace {
             IPHostEntry Host = Dns.GetHostEntry(args[0]);
             IPAddress destIP = Host.AddressList[0];
 
-            // send a ping to destination
-            int t = 1; // set ttl
-            PingReply ping = SendPing(destIP, t);
+            // create list
+            List<IPAddress> PingResults = new();
+
+            // set initial ttl
+            int t = 1;
+
+            do {
+                // send a ping to destination
+                PingReply ping = SendPing(destIP, t);
+
+                if (ping == null) {
+                    Console.WriteLine("No return packet received");
+                }
+                // ping makes it to destination
+                else if (ping.Status == IPStatus.Success) {
+                    PingResults.Add(ping.Address);
+                }
+                // ping is dropped because ttl is too low
+                else if (ping.Status == IPStatus.TimedOut || ping.Status == IPStatus.TtlExpired || ping.Status == IPStatus.TimeExceeded) {
+                    PingResults.Add(ping.Address);
+                    t++;
+                }
+                // all other cases
+                else {
+                    Console.WriteLine("Unable to perform traceroute");
+                }
+            }
+            while (!PingResults.Contains(destIP));
+
+            // write out results
+            int index = 0;
+            foreach (IPAddress ip in PingResults) {
+                Console.WriteLine($"{index} {ip}");
+                index++;
+            }
         }
 
         // ping method
