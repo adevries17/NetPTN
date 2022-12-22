2f60b74702d575dcf83f5e4914ab5034e9fd6277
adevries17 <andrew@andrewdevries.net>
2022-12-22 11:53:15 -0600

    begin gui design

--- NetGUI/NetGUIForm.Designer.cs
+++ NetGUI/NetGUIForm.Designer.cs
@@ -23,10 +23,21 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // NetGUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "NetGUIForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NetPTN";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
